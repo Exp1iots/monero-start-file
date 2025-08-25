a36850b876077a37aa8cf9d7fa9443c636d81617
Exp1oits <154223131+Exp1iots@users.noreply.github.com>
2025-08-25 18:04:13 +1000

    Update start_mining.sh

--- start_mining.sh
+++ start_mining.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./xmrig/build/xmrig -o gulf.moneroocean.stream:10128 -u 4A3D3kTZbDpNJeMhJaz21PQChqgSisWv1aYPNSF5Sp659EhgmHvVXrr96qMRwrapMVh2F9rmu78Z7bS3ft3hEzQjJEb7sHi -p ryzen5_3600
+./xmrig/build/xmrig -o gulf.moneroocean.stream:10128 -u <wallet_address> -p <name>
